6cbec704dcc101e0617f1c09363f221928ae1ad8
yashsongire225 <yashsongire225@gmail.com>
2026-08-18 16:46:31 +0530

    feat: add classification report to training script

--- src/train.py
+++ src/train.py
@@ -28,6 +28,7 @@ def train_model(X_train, y_train, n_estimators=100, max_depth=None):
 
 def evaluate_model(model, X_test, y_test):
     predictions = model.predict(X_test)
+    print(classification_report(y_test, predictions))
     acc = accuracy_score(y_test, predictions)
     report = classification_report(y_test, predictions)
     return acc, report
